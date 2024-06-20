9c3d096a4aa67648461154b1aa0047558c3d5893
Eduardo Gimeno <eduardo.gimeno@estudiantes.utec.edu.uy>
2024-06-20 17:42:23 -0300

    Refactor user dropdown component to remove image dependency

--- src/components/Header/DropdownUser.tsx
+++ src/components/Header/DropdownUser.tsx
@@ -73,21 +73,13 @@ const DropdownUser = () => {
               <span className="block text-sm font-medium text-black dark:text-white">
                 Invitado
               </span>
-              <span className="block text-xs">UX Designer</span>
+              <span className="block text-xs">Invitado</span>
             </span>
             <span className="h-12 w-12 rounded-full">
-              <Image
-              className="rounded-full"
-                width={112}
-                height={112}
-                src={"/images/user/user-01.png"}
-                style={{
-                  width: "auto",
-                  height: "auto",
-                  borderRadius: "50%",
-                }}
-                alt="User"
-              />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-12">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+</svg>
+
             </span>
           </>)}
 
