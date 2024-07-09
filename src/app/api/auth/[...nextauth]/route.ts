5fa91b1bac610fda3fff5dd25042b07722633c3e
Eduardo Gimeno <eduardo.gimeno@estudiantes.utec.edu.uy>
2024-07-09 02:07:12 -0300

    debug logica de ingeso/registro

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -56,6 +56,7 @@ const handler = NextAuth({
 
 
         const data = await res.json();
+        console.log(data);
 
 if (res.ok && data && data.user.estado === "ACTIVO") {
   // Transformar los datos según el formato deseado
@@ -76,6 +77,7 @@ if (res.ok && data && data.user.estado === "ACTIVO") {
   return transformedData;
 } else if (res.ok && data && data.user.estado === "INACTIVO") { 
   //mostrar mensaje de cuenta inactiva
+  console.log("Cuenta inactiva, por favor contacte al administrador");
   return alert("Cuenta inactiva, por favor contacte al administrador");
 }else {
   return null;
@@ -88,7 +90,7 @@ if (res.ok && data && data.user.estado === "ACTIVO") {
     signIn: "/auth/signin",
   },
   callbacks: {
-    async signIn({ user, account, profile }) {
+    async signIn({ user, account, profile }): Promise<any> {
       if (account?.provider === "google") {
         const res = await fetch(`http://localhost:8080/ServidorApp-1.0-SNAPSHOT/api/usuarios/google-login`, {
           method: 'POST',
@@ -96,65 +98,31 @@ if (res.ok && data && data.user.estado === "ACTIVO") {
           body: JSON.stringify({ email: profile?.email, name: profile?.name }),
         });
         const data = await res.json();
-        if(res.ok && data.user.estado === "INACTIVO") { 
-          //mostrar mensaje de cuenta inactiva
+        console.log(data);
+        if (data.userNeedsAdditionalInfo) {
+          return `/auth/signup?email=${profile?.email}`;
+        }
+        if(res.ok && data.user.estado === "INACTIVO") {
           error("Cuenta inactiva, por favor contacte al administrador");
-          
-          
-          return false;
-        }else if(res.ok && data.user.estado === "ACTIVO") {
-          console.log("Datos recibidos de la API de Google login:", data);
-          if (data.userNeedsAdditionalInfo) {
-            return `/auth/signup?email=${profile?.email}`;
-          }
-          user.accessToken = data.token;
-          user.data = data.user;
-          return { ...user.data, accessToken: data.token };
-        }else {
+          console.log("Cuenta inactiva, por favor contacte al administrador");
           return false;
         }
+        user.accessToken = data.token;
+        user.data = data.user;
+        return { ...user.data, accessToken: data.token };
+      } else {
+        return false;
       }
-      return true;
     },
 
     async jwt({ token, user }) {
       if (user) {
-        console.log("Datos del usuario en jwt callback:", user);
-        //token.accessToken = user.accessToken;
-        token.user = {
-          ...user,
-          id: user.id,
-          cedula: user.cedula,
-          contrasenia: user.contrasenia,
-          fechaNacimiento: user.fechaNacimiento,
-          estado: user.estado,
-          nombre: user.nombre,
-          apellido: user.apellido,
-          nombreUsuario: user.nombreUsuario,
-          idInstitucion: user.idInstitucion,
-          idPerfil: user.idPerfil,
-          usuariosTelefonos: user.usuariosTelefonos
-        };
+        token.user = user;
       }
       return token;
     },
     async session({ session, token }) {
-      console.log("Datos del token en session callback:", token);
-      //session.accessToken = token.accessToken as string;
-      session.user = {
-        ...token.user,
-        id: token.user.id,
-        cedula: token.user.cedula,
-        contrasenia: token.user.contrasenia,
-        fechaNacimiento: token.user.fechaNacimiento,
-        estado: token.user.estado,
-        nombre: token.user.nombre,
-        apellido: token.user.apellido,
-        nombreUsuario: token.user.nombreUsuario,
-        idInstitucion: token.user.idInstitucion,
-        idPerfil: token.user.idPerfil,
-        usuariosTelefonos: token.user.usuariosTelefonos,
-      };
+      session.user = token.user;
       return session;
     }
   },
