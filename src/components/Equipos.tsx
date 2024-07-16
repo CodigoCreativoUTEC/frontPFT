04e8964b99233935022372532ad56561cfcb4dee
Eduardo Gimeno <eduardo.gimeno@estudiantes.utec.edu.uy>
2024-07-16 00:37:17 -0300

    muestra datos de equipos

--- src/components/Equipos.tsx
+++ src/components/Equipos.tsx
@@ -11,23 +11,24 @@ const EquiposList: React.FC<EquiposListProps> = (params) => {
     <tr className="bg-gray-2 text-center dark:bg-meta-4 ">
       <td className='px-2 py-2 font-medium text-black dark:text-white'>{params.id}</td>
       <td className=' px-2 py-2 font-medium text-black dark:text-white'>{params.nombre}</td>
-      <td className='px-2 py-2 font-medium text-black dark:text-white'>{params.tipo_equipo}</td>
-      <td className='px-2 py-2 font-medium text-black dark:text-white'>{params.marca}</td>
-      <td className='px-2 py-2 font-medium text-black dark:text-white'>{params.modelo}</td>
-      <td className='px-2 py-2 font-medium text-black dark:text-white'>{params.num_serie}</td>
+      <td className='px-2 py-2 font-medium text-black dark:text-white'>{params.idTipo.nombreTipo}</td>
+      <td className='px-2 py-2 font-medium text-black dark:text-white'>{params.idModelo.nombre}</td>
+      <td className='px-2 py-2 font-medium text-black dark:text-white'>{params.idModelo.idMarca.nombre}</td>
+      <td className='px-2 py-2 font-medium text-black dark:text-white'>{params.nroSerie}</td>
       <td className='px-2 py-2 font-medium text-black dark:text-white'>{params.garantia}</td>
-      <td className='px-2 py-2 font-medium text-black dark:text-white'>{params.pais}</td>
-      <td className='px-2 py-2 font-medium text-black dark:text-white'>{params.proveedor}</td>
-      <td className='px-2 py-2 font-medium text-black dark:text-white'>{new Date(params.fecha_adq).toLocaleDateString()}</td>
-      <td className='px-2 py-2 font-medium text-black dark:text-white'>{params.id_interno}</td>
-      <td className='px-2 py-2 font-medium text-black dark:text-white'>{params.ubicacion}</td>
-      <td className='px-2 py-2 font-medium text-black dark:text-white'>{params.imagen}</td>
+      <td className='px-2 py-2 font-medium text-black dark:text-white'>{params.idPais.nombre}</td>
+      <td className='px-2 py-2 font-medium text-black dark:text-white'>{params.idProveedor.nombre}</td>
+      <td className='px-2 py-2 font-medium text-black dark:text-white'>{new Date(params.fechaAdquisicion).toLocaleDateString()}</td>
+      <td className='px-2 py-2 font-medium text-black dark:text-white'>{params.idInterno}</td>
+      <td className='px-2 py-2 font-medium text-black dark:text-white'>{params.idUbicacion.nombre}/{params.idUbicacion.sector}</td>
+      <td className='px-2 py-2 font-medium text-black dark:text-white'><img src={params.imagen} height={150} width={150}/></td>
       <td className='px-2 py-2 font-medium text-black dark:text-white'>{params.estado}</td>
       <td className='px-2 py-2 font-medium text-black dark:text-white'>
+      <div className="inline-flex">
         <span>
           <Link 
             href={`/equipos/delete/${params.id}`}
-            className='bg-rose-500 p-2 inline-block ml-3 text-white text-xs rounded cursor-pointer'
+            className='bg-rose-500 p-2 inline-block ml-1 text-white text-xs rounded cursor-pointer'
           >
             Borrar
           </Link>
@@ -35,17 +36,20 @@ const EquiposList: React.FC<EquiposListProps> = (params) => {
         <span>
           <Link 
             href={`/equipos/edit/${params.id}`}
-            className='bg-yellow-500 p-2 inline-block ml-3 text-white text-xs rounded cursor-pointer'
+            className='bg-yellow-500 p-2 inline-block ml-1 text-white text-xs rounded cursor-pointer'
           >
             Editar
           </Link>
         </span>
+        <span>
         <Link 
           href={`/equipos/read/${params.id}`}
-          className='bg-blue-500 p-2 inline-block ml-3 text-white text-xs rounded cursor-pointer'
+          className='bg-blue-500 p-2 inline-block ml-1 text-white text-xs rounded cursor-pointer'
         >
           Ver
         </Link>
+        </span>
+        </div>
       </td>
     </tr>
   );
