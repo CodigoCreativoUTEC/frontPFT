ff1b872872906e6850436ed824359b1a45c89e0b
Eduardo Gimeno <eduardo.gimeno@estudiantes.utec.edu.uy>
2024-09-13 18:23:45 -0300

    Menu en reparacion

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -5,17 +5,25 @@ import { usePathname } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
 import SidebarLinkGroup from "./SidebarLinkGroup";
+import { Session } from "inspector";
+import { signIn, useSession } from 'next-auth/react';
 
 interface SidebarProps {
   sidebarOpen: boolean;
   setSidebarOpen: (arg: boolean) => void;
 }
+// Función para verificar permisos
+function hasPermission(userProfile: string, requiredPermissions: string[]): boolean {
+  return requiredPermissions.includes(userProfile);
+}
 
 const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
+  
   const pathname = usePathname();
 
   const trigger = useRef<any>(null);
   const sidebar = useRef<any>(null);
+  const { data: session, status } = useSession();
 
   let storedSidebarExpanded = "true";
 
@@ -58,6 +66,8 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
     }
   }, [sidebarExpanded]);
 
+  const userProfile = session?.user?.perfil || '';
+
   return (
     <aside
       ref={sidebar}
@@ -98,7 +108,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
           {/* <!-- Menu Group --> */}
           <div>
             <h3 className="mb-4 ml-4 text-sm font-semibold text-bodydark2">
-              MENU
+              MENU <aside>{session?.user.perfil}</aside>
             </h3>
 
             <ul className="mb-6 flex flex-col gap-1.5">
@@ -111,53 +121,68 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                 {(handleClick, open) => {
                   return (
                     <React.Fragment>
-                      <Link
-                        href="#"
-                        className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${(pathname === "/" ||
-                            pathname.includes("dashboard")) &&
-                          "bg-graydark dark:bg-meta-4"
-                          }`}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          sidebarExpanded
-                            ? handleClick()
-                            : setSidebarExpanded(true);
-                        }}
-                      >
-
-                        Escritorio
-                        <svg
-                          className={`absolute right-4 top-1/2 -translate-y-1/2 fill-current ${open && "rotate-180"
-                            }`}
-                          width="20"
-                          height="20"
-                          viewBox="0 0 20 20"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            fillRule="evenodd"
-                            clipRule="evenodd"
-                            d="M4.41107 6.9107C4.73651 6.58527 5.26414 6.58527 5.58958 6.9107L10.0003 11.3214L14.4111 6.91071C14.7365 6.58527 15.2641 6.58527 15.5896 6.91071C15.915 7.23614 15.915 7.76378 15.5896 8.08922L10.5896 13.0892C10.2641 13.4147 9.73651 13.4147 9.41107 13.0892L4.41107 8.08922C4.08563 7.76378 4.08563 7.23614 4.41107 6.9107Z"
-                            fill=""
-                          />
-                        </svg>
-                      </Link>
                       {/* <!-- Dropdown Menu Start --> */}
-                      <div
-                        className={`translate transform overflow-hidden ${!open && "hidden"
-                          }`}
-                      >
+                      <div className={`translate transform overflow-hidden ${!open && "hidden" }`}>
                         <ul className="mb-5.5 mt-4 flex flex-col gap-2.5 pl-6">
                           <li>
                             <Link
                               href="/"
                               className={`group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ${pathname === "/" && "text-white"
-                                }`}
-                            >
-                              Principal
+                                }`}>
+                              Escritorio
                             </Link>
                           </li>
+
+
+
+
+
+{/* Menú para Administradores */}
+{hasPermission(userProfile, ["Administrador", "Aux administrativo"]) && (
+              <SidebarLinkGroup activeCondition={pathname === "/" || pathname.includes("/")}>
+                {(handleClick, open) => (
+                  <>
+                    <div>
+                      <Link href="/admin" className="group flex items-center px-4 py-2">Admin Dashboard</Link>
+                    </div>
+                  </>
+                )}
+              </SidebarLinkGroup>
+            )}
+
+            {/* Menú para Técnicos */}
+            {hasPermission(userProfile, ["Técnico", "Administrador"]) && (
+              <SidebarLinkGroup activeCondition={pathname === "/equipos" || pathname.includes("equipos")}>
+                {(handleClick, open) => (
+                  <>
+                    <div>
+                      <Link href="/equipos" className="group flex items-center px-4 py-2">Ver Equipos</Link>
+                    </div>
+                    <div>
+                      <Link href="/equipos/create" className="group flex items-center px-4 py-2">Agregar Equipos</Link>
+                    </div>
+                  </>
+                )}
+              </SidebarLinkGroup>
+            )}
+
+            {/* Menú para Usuarios comunes */}
+            {hasPermission(userProfile, ["Tecnico", "Administrador"]) && (
+              <SidebarLinkGroup activeCondition={pathname === "/usuarios" || pathname.includes("usuarios")}>
+                {(handleClick, open) => (
+                  <>
+                    <div>
+                      <Link href="/usuarios" className="group flex items-center px-4 py-2">Ver Usuarios</Link>
+                    </div>
+                  </>
+                )}
+              </SidebarLinkGroup>
+            )}
+
+
+
+
+
                           <li>
                             <SidebarLinkGroup
                               activeCondition={
