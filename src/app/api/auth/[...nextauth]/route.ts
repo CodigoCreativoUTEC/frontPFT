92dfb64b8ed03af015ec80f82e384626f760e6e0
Eduardo Gimeno <eduardo.gimeno@estudiantes.utec.edu.uy>
2024-10-07 20:39:44 -0300

    Cambio para el login simple

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -58,7 +58,7 @@ const handler = NextAuth({
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
-            usuario: credentials?.usuario,
+            email: credentials?.usuario,
             password: hashedPassword
           })
         });
