ccf45148e6453714db7bd9460d33847b00a08233
todobanners <ecgb2007@gmail.com>
2024-11-09 00:19:03 -0300

    quite hash

--- src/components/Usuarios/crearUsuario.tsx
+++ src/components/Usuarios/crearUsuario.tsx
@@ -5,16 +5,6 @@ import { useRouter, useSearchParams } from 'next/navigation';
 import { useState, useEffect } from 'react';
 import { signIn, useSession } from 'next-auth/react';
 
-// Función para generar el hash SHA-256
-const hashPassword = async (password) => {
-    const encoder = new TextEncoder();
-    const data = encoder.encode(password);
-    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
-    return hashHex;
-};
-
 export default function Registrar() {
     const { data: session } = useSession();
 
@@ -126,8 +116,6 @@ export default function Registrar() {
         if (!validate()) {
             return;
         }
-        // Hashear la contraseña antes de enviarla
-        const hashedPassword = await hashPassword(formData.contrasenia);
 
         const usuarioDto = {
             ...formData,
