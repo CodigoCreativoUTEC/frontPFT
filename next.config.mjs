e432f9e8e8be7ec850e1a1a058f1b7382f6c70b9
Eduardo Gimeno <eduardo.gimeno@estudiantes.utec.edu.uy>
2025-07-08 02:20:50 -0300

    Remove unused files and components, including various pages and types related to users, equipos, funcionalidades, marcas, modelos, and proveedores. Update .gitignore to exclude new files. Bump package version in package-lock.json and package.json. Clean up project structure by deleting unnecessary configuration files.

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['ui-avatars.com','lh3.googleusercontent.com'],
-    },
-  };
-  
-  export default nextConfig;
-/** @type {import('next').NextConfig} */
-const nextConfig = {
   images: {
     remotePatterns: [
       {
